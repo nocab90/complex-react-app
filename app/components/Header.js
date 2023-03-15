dc3d6a1805b25c9ed23cdab2ecd429602e5e7814
Kagen Chow <kc@email.com>
2023-03-15 16:17:01 +0800

    Conditional Homepage

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(
-    Boolean(localStorage.getItem("complexappToken"))
-  );
-
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -17,10 +13,10 @@ function Header() {
             ComplexApp{" "}
           </Link>
         </h4>
-        {loggedIn ? (
-          <HeaderLoggedIn setLoggedIn={setLoggedIn} />
+        {props.loggedIn ? (
+          <HeaderLoggedIn setLoggedIn={props.setLoggedIn} />
         ) : (
-          <HeaderLoggedOut setLoggedIn={setLoggedIn} />
+          <HeaderLoggedOut setLoggedIn={props.setLoggedIn} />
         )}
       </div>
     </header>
