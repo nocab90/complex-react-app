c169ab899a207a2149799e43e84a041153116049
Kagen Chow <kc@email.com>
2023-03-15 10:32:20 +0800

    Setting up React components

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Footer from "./components/Footer";
 
-function ExampleComponent() {
+//My components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+
+function Main() {
   return (
-    <div>
-      <h1>This is our app!!!</h1>
-      <p>The sky is blue and the grass is green.</p>
-    </div>
+    <>
+      <Header />
+
+      <HomeGuest />
+
+      <Footer />
+    </>
   );
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#app"));
-root.render(<ExampleComponent />);
+root.render(<Main />);
 
+//Allows the browser to update page contents without refreshing
 if (module.hot) {
   module.hot.accept();
 }
