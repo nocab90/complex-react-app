aa6beceae52b1aa9388364aac54bb1dc92ed603d
Kagen Chow <kc@email.com>
2023-03-15 17:08:13 +0800

    Axios baseUrl + Post routings

--- app/Main.js
+++ app/Main.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//Setting up baseUrl for Axios requests globally
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
+
 //My components
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -9,6 +13,8 @@ import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(
@@ -21,8 +27,10 @@ function Main() {
 
       <Routes>
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/create-post" element={<CreatePost />}></Route>
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/post/:id" element={<ViewSinglePost />}></Route>
       </Routes>
 
       <Footer />
