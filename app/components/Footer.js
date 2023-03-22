c102fd2c448e34b8a33a3b83c9624d7df278c181
Kagen Chow <kc@email.com>
2023-03-22 11:05:44 +0800

    Load Posts

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
         </Link>
       </p>
       <p className="m-0">
-        Copyright &copy; {new Date().getFullYear()}
+        Copyright &copy; {new Date().getFullYear()}{" "}
         <a href="/" className="text-muted">
           ComplexApp
         </a>
